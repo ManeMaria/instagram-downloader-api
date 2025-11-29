42085ced0ea1218713cd49139ad09aaff27bde30
ManeMaria <mirandacesar598@gmail.com>
2025-11-28 22:34:57 -0300

    test: add end-to-end tests for valid and invalid Instagram download URLs with error handling

--- __tests__/downloader.spec.ts
+++ __tests__/downloader.spec.ts
@@ -3,9 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
+import { DownloadUsecase } from 'src/usecases/instagram/download.usecase';
 
 const validUrl =
   'https://www.instagram.com/reel/DRJ224UjmO3/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==';
+
 describe('DownloaderController (e2e)', () => {
   let app: INestApplication<App>;
 
@@ -24,6 +26,9 @@ describe('DownloaderController (e2e)', () => {
 
   describe('valid url', () => {
     it('should return 200', async () => {
+      const expectedResponse = { url: 'test' };
+      jest.spyOn(DownloadUsecase.prototype, 'execute').mockResolvedValueOnce(Promise.resolve(expectedResponse.url));
+
       const response = await request(app.getHttpServer())
         .post('/instagram/download')
         .send({
@@ -31,18 +36,57 @@ describe('DownloaderController (e2e)', () => {
         });
 
       expect(response.status).toBe(200);
+      expect(response.body).toEqual(expectedResponse);
+
     });
   });
 
   describe('invalid url', () => {
     it('should return 400', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/instagram/download')
-        .send({
-          url: 'invalid-url',
-        });
+      try {
+        await request(app.getHttpServer())
+          .post('/instagram/download')
+          .send({
+            url: 'invalid-url',
+          });
 
-      expect(response.status).toBe(400);
+      } catch (error) {
+        expect(error.body.message).toBe('url não é uma url do instagram');
+        expect(error.status).toBe(400);
+        expect(error.body.error).toBe('Bad Request');
+      }
+    });
+  });
+
+  describe('falsy value property', () => {
+    it('should return 400', async () => {
+      try {
+        await request(app.getHttpServer())
+          .post('/instagram/download')
+          .send({
+            url: undefined,
+          });
+      } catch (error) {
+        expect(error.status).toBe(400);
+        expect(error.body.message).toBe('somente texto');
+        expect(error.body.error).toBe('Bad Request');
+      }
+    });
+  });
+
+  describe('null value property', () => {
+    it('should return 400', async () => {
+      try {
+        await request(app.getHttpServer())
+          .post('/instagram/download')
+          .send({
+            url: '',
+          });
+      } catch (error) {
+        expect(error.status).toBe(400);
+        expect(error.body.message).toBe('não é permitido propriedade vazia');
+        expect(error.body.error).toBe('Bad Request');
+      }
     });
   });
 });
